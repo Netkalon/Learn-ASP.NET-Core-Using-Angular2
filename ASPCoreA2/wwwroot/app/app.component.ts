5407d5b07b80ad49545f793569ce01a22d39fb39
Myn <Myn@MYLTOP>
2017-07-10 00:47:46 +0530

    Add project files.

---
+++ ASPCoreA2/wwwroot/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core'  
+@Component({  
+    selector: 'app-loader',  
+    template: `  
+<div>   
+<div>  
+ <h4>Welcome to ASP.NET Core Using angular 2 in Visual Studio 2017</h4>
+</div></div>
+`  
+})  
+export class AppComponent{}  
